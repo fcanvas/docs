109ddce8f6d032a28af9c460626c63202bf070cf
tachibana-shin <tachibshin@duck.com>
2023-03-09 02:56:07 +0000

    Deploying to gh-pages from @ fcanvas/docs@3c58ad1cb1d4f9281ab69a9998e739269fb502e7 🚀

--- assets/guide_shapes_Tag.md.a437d2f3.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o as e,c as a,b as d}from"./app.90e2b169.js";const g=JSON.parse('{"title":"Tag Shape","description":"","frontmatter":{},"headers":[],"relativePath":"guide/shapes/Tag.md","lastUpdated":1678330269000}'),o={name:"guide/shapes/Tag.md"},r=d("",8),s=[r];function i(n,c,h,p,l,u){return e(),a("div",null,s)}const b=t(o,[["render",i]]);export{g as __pageData,b as default};
